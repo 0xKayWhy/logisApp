72d1e57ae783f25363ce26b37e37bde9bce4a42c
kyCheong <cky_2233@hotmail.com>
2024-08-17 22:50:22 +0800

    fixed 404 bug

--- logistic-app/src/component/notFound/index.jsx
+++ logistic-app/src/component/notFound/index.jsx
@@ -12,8 +12,8 @@ export const NotFound = () => {
       <h1 className="fst-italic font-monospace fw-bold mb-5">
         404 - Page Not Found
       </h1>
-      <NavLink to={`/${role}`}>
-      <Button>Back to {role === "deliveryguy" ? "Delivery" : "Admin"}</Button>
+      <NavLink to={`/${role ? role : ""}`}>
+      <Button>Back to {role ? (role === "deliveryguy" ? "Delivery" : "Admin") : "Home"}</Button>
       </NavLink>
       <Image className="mt-5" src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYW11bnE0djkyaGs3NWdhMXRna2Y0dzExd2VjMXRydjZ6amhrbTc0ZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Ju7l5y9osyymQ/giphy.gif" alt="rickRolled"/>
     </div>
